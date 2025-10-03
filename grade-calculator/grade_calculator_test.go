0fd54e3a25896202d494d6750b9031b625181146
sdehaarte <redzen77@gmail.com>
2025-10-02 22:41:23 -0400

    updated TestGetGradeF with inputs below 60 that will sucessfully apply a letter grade F

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -39,9 +39,9 @@ func TestGetGradeF(t *testing.T) {
 
 	gradeCalculator := NewGradeCalculator()
 
-	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
-	gradeCalculator.AddGrade("exam 1", 95, Exam)
-	gradeCalculator.AddGrade("essay on ai ethics", 91, Essay)
+	gradeCalculator.AddGrade("open source assignment", 39, Assignment)
+	gradeCalculator.AddGrade("exam 1", 37, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 33, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
 
